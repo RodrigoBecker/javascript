1d63f010e11353f42086b9667746554cccde5e25
RodrigoBecker <rodrigo.more@totvs.com.br>
2019-01-02 17:39:58 -0200

    Realizando operações com o javascript

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,29 @@
-alert("Olá Mundo!")
-	var titulo = (document.querySelector(".titulo")); // Buscando elemento pela sua classe 
+/*alert("Olá Mundo!")
+    var titulo = (document.querySelector(".titulo")); // Buscando elemento pela sua classe 
+    var subtitulo = (document.querySelector("#subtitulo")); // Buscando elemento pelo sey id 
 	console.log(titulo);
-	console.log(titulo.textContent); 
+    console.log(titulo.textContent); 
+    console.log(subtitulo.textContent);
+*/
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+
+// Peso
+var tdPeso =  paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+// Altura
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+
+/*
+console.log(paciente); // tr peso
+console.log(tdPeso); // td com o peso 
+console.log(peso); // Obtem o valor do peso 
+console.log(altura) // Obtem altura
+*/
+
+var imc = peso / (altura * altura); //Calculo de javascript usando ordem de Precedência 
+console.log(imc); // imprime resultado 
