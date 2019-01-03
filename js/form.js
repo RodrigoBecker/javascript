74a0835a3a8f09aea081dc849c841eb9386e469d
RodrigoBecker <rodrigo.more@totvs.com.br>
2019-01-03 20:41:24 -0200

    Melhorando ainda mais o código

--- js/form.js
+++ js/form.js
@@ -5,26 +5,61 @@ botaoAdicionar.addEventListener("click", function (event) {
     //qconsole.log("Oi eu sou o botão")
     var form = document.querySelector("#form-adiciona");
 
+
+
+    // Extrai informações do form para adicionar um novo paciente
+    var paciente = obtemPaciente(form);
+
+
+    console.log(paciente);
+
+
+    // variavel que recebe dados da função 
+    var pacienteTr = montaTr(paciente);
+
+    //Adicionando o paciente na tabela 
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
+
+
+function obtemPaciente(form) {
+
+    //objeto paciente
+    var Paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaIMC(form.peso.value, form.altura.value)
+
+    }
+
+
+    /*
     var nome = form.nome.value; // resgatando do objeto form o valor do input atraves da propriedade .peso.value
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
-    
-     //console.log(form.altura.value);
+    */
 
-     var pacienteTr = document.createElement("tr"); // utilizando a função createElement para construir elementos html através do javascript
-     
-     var nomeTd = document.createElement("td");
-     var pesoTd = document.createElement("td");
-     var alturaTd = document.createElement("td");
-     var gorduraTd = document.createElement("td");
-     var imcTd = document.createElement("td");
-     
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaIMC(peso,altura);
+    return Paciente;
+
+}
+
+function montaTr(paciente) {
+
+    var pacienteTr = document.createElement("tr"); // utilizando a função createElement para construir elementos html através do javascript
+    pacienteTr.classList.add("paciente");
+
+
+    var nomeTd = montaTd(paciente.nome, "info-nome");
+    var pesoTd = montaTd(paciente.peso, "info-peso");
+    var alturaTd = montaTd(paciente.altura, "info-altura");
+    var alturaTd = montaTd(paciente.altura, "info-altura");
+    var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+    var imcTd = montaTd(paciente.imc, "info-imc");
+
 
 
     pacienteTr.appendChild(nomeTd);
@@ -32,7 +67,15 @@ botaoAdicionar.addEventListener("click", function (event) {
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
-    
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-});
+
+
+    return pacienteTr;
+}
+
+function montaTd (dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
