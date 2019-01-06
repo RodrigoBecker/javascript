5f5f5a94e3cbbc1f2f9a76ade5bf7f62f345c942
RodrigoBecker <rodrigo.more@totvs.com.br>
2019-01-06 21:39:27 -0200

    Trabalhando com filtro e expressão regular

---
+++ js/filtrar.js
@@ -0,0 +1,38 @@
+var CampoFiltro = document.querySelector("#filtrar-tabela");
+//console.log(CampoFiltro);
+
+CampoFiltro.addEventListener("input", function () {
+    //console.log("Estou digitando");
+    //console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+
+    if (this.value.length > 0) {
+        console.log("Tem algo digitado");
+
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i]
+            var TdNome = paciente.querySelector(".info-nome");
+            var nome = TdNome.textContent;
+            var Expressao = RegExp(this.value, "i")
+            //console.log(nome);
+            if (!Expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel")
+            }
+
+        }
+
+
+    } else {
+        for(var i = 0 ; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+
+
+
+});
